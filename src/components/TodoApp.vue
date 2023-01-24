8891971956f920008c891ebe1270b2703b4d1be4
cakradana <rcakradana@gmail.com>
2023-01-24 14:57:25 +0700

    first commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="todo-app">
+      <h1>Todo List</h1>
+      <todo-form />
+      <todo-list />
+   </div>
+</template>
+
+<script setup>
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
+</script>
+
+<style scoped>
+.todo-app {
+   padding: 5vh;
+   min-height: 100vh;
+   width: 30vw;
+   background-color: #f7f9fc;
+}
+</style>
